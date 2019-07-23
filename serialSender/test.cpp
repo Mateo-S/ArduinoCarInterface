61c223d4b5b3b6df948968a13fa6dab02257dce3
Mateo Silver <mateonsilver@gmail.com>
2019-07-23 10:25:57 -0500

    Adding printouts

--- serialSender/test.cpp
+++ serialSender/test.cpp
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <limits.h>
 #include "serialSender.h"
+#include <iostream>
 
 int main(int argc, char* argv[]){
 
@@ -15,17 +16,21 @@ int main(int argc, char* argv[]){
         car = new SerialSender(std::string(argv[1]));
     }
     else {
-        std::cerr << "Usage: ./test [device]" << std::endl;
+        std::cerr << "Usage: ./a.out [device]" << std::endl; //on jetson device is /dev/ttyS0
         exit(1);
     }
 
     usleep(2000);
     // std::thread::sleep_for(std::chrono::seconds(2));
-    car -> updateSteering(SHRT_MAX - (SHRT_MAX/10)); //right
+    car -> updateSteering(SHRT_MAX - (SHRT_MAX/10)); // 9/10 right
+    std::cout << "Sending: " + (SHRT_MAX - (SHRT_MAX/10)) << "\n";
+    
     usleep(2000);
     // std::thread::sleep_for(std::chrono::seconds(2));
-    car -> updateSteering(SHRT_MIN - (SHRT_MIN/10)); //left
+    car -> updateSteering(SHRT_MIN - (SHRT_MIN/10)); // 9/10 left
+    std::cout << "Sending: " + (SHRT_MIN - (SHRT_MIN/10)) << "\n";
     usleep(2000);
     // std::thread::sleep_for(std::chrono::seconds(2));
     car -> updateSteering(0); //middle
+    std::cout << "Sending: " + 0 << "\n";
 }
